45e302c4970492402a46d071c358266eebd14969
chris-daynes <chrisdaynes@gmail.com>
2016-12-10 12:03:50 +1300

    added working map and pins

--- services/map.js
+++ services/map.js
@@ -1,1 +1,32 @@
-// KEY = 'AIzaSyDNqZpfY5wCQjq78QqttpZJ05714XxQTuI'
+import React from 'react'
+import { GoogleMapLoader, GoogleMap, Marker } from 'react-google-maps'
+
+
+const Map = ({state}) => {
+  const { location, markers } = state
+  const mapContainer = <div style={{height:'100%', width:'100%'}}></div>
+
+  const pins = markers.map((venue, i) => {
+    const marker = {
+      position: {
+        lat: venue.lat,
+        lng: venue.lng
+      }
+    }
+    return <Marker key={i} {...marker} />
+  })
+console.log('these are the pins ', pins);
+return (
+  <GoogleMapLoader
+    containerElement = { mapContainer }
+    googleMapElement = {
+      <GoogleMap
+        defaultZoom={15}
+        defaultCenter={location}
+        options={{streetViewControl: false, mapTypeControl: false }}>
+        { pins }
+      </GoogleMap>
+    } />
+)
+}
+ export default Map
