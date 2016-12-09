d7ba81f392cb85bdcc3c1ca853fb4e9b6ed35b90
bradymat <bradymat@gmail.com>
2016-12-10 11:06:53 +1300

    removed google maps from yo-yo

--- services/map.js
+++ services/map.js
@@ -1,17 +1,1 @@
-import GoogleMapsLoader from 'google-maps'
-
-GoogleMapsLoader.KEY = 'AIzaSyDNqZpfY5wCQjq78QqttpZJ05714XxQTuI'
-GoogleMapsLoader.LIBRARIES = ['geometry', 'places']
-GoogleMapsLoader.REGION = 'GB'
-
-var center = {lat: 40.7128, lng: 74.0059}
-
-module.exports = (el) => {
-  GoogleMapsLoader.load((google) => {
-    new google.maps.Map(el, {
-      center,
-      // location: '275 cuba st wellington nz',
-      zoom: 4
-    })
-  })
-}
+// KEY = 'AIzaSyDNqZpfY5wCQjq78QqttpZJ05714XxQTuI'
