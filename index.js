fbc4c747659c3085807f4e09440fac37772815db
JasmineGWT <jazzgwwt@gmail.com>
2016-12-11 00:40:50 +1300

    Updates

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import Router from 'sheet-router'
+import request from 'superagent'
 
 import reducer from './reducer'
 
@@ -11,7 +12,6 @@ import Play from './components/play'
 import MyMap from './components/mymap'
 import Adventure from './components/myAdventure'
 import getAdventures from './services/getAdventure'
-
 
 const initState = {
   title: 'Beercompass',
@@ -34,7 +34,21 @@ navigator.geolocation.watchPosition((position) => {
     lat: position.coords.latitude,
     lng: position.coords.longitude
   }
-  dispatch({type:'UPDATE_CURRENT_POS' , payload: pos})
+       console.log('I checked if you were at a new place')      
+  
+  request
+    .get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${pos.lat},${pos.lng}&key=AIzaSyDNqZpfY5wCQjq78QqttpZJ05714XxQTuI`)
+    .end((err, res) => {
+      var placeId = res.body.results[0].place_id
+      pos.placeId = placeId
+      var check = getState().markers.find((place) => {
+        return place.placeId == placeId
+      })
+      if(!check){ 
+        console.log('I put the place in the state', placeId)
+        dispatch({type:'UPDATE_CURRENT_POS' , payload: pos})
+      }
+    })
 })
 
 getAdventures(dispatch)
