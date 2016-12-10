3e3103fea31fb2cc2444f8bc6f113269d8e61508
bradymat <bradymat@gmail.com>
2016-12-10 14:15:54 +1300

    Updates

---
+++ index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import Router from 'sheet-router'
+
+import reducer from './reducer'
+
+import Login from './components'
+import SignUp from './components/signUp'
+import Play from './components/play'
+import MyMap from './components/mymap'
+
+const initState = {
+  title: 'Beercompass',
+  route: '/',
+  lastRoute: 'lemon',
+  loginDetails: {},
+  newUserDetails: {},
+  location: {lat:-41.2966371, lng:174.77446609999998},
+  markers: [{
+          lat:-50.2966480,
+          lng:176.77446609999998
+        }, {
+          lat:-41.2966371,
+          lng:174.77446609999998
+        }
+    ]
+}
+
+const { getState, dispatch, subscribe } = createStore(reducer, initState)
+
+const route = Router({ default: '/404' }, [
+  ['/', (params) => Login],
+  ['/signUp', (params) => SignUp],
+  ['/play', (params) => Play],
+  ['/mymap', (params) => MyMap]
+])
+
+subscribe(() => {
+  var Component = route(getState().route)
+  render(<Component state={getState()} dispatch={dispatch}/> , document.querySelector('main'))
+})
+
+dispatch({type: 'lemon'})
