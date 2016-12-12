d1c04d876cee110b4a34a137d104ef544b06d0c1
Steve Sharp <zimsteve@live.com>
2016-12-12 20:39:35 +1300

    Added drink responsibly and extra sass

--- components/loading.js
+++ components/loading.js
@@ -5,6 +5,7 @@ module.exports = ({ state, dispatch }) => {
   return (
     <div className='loading'>
       <Header state={state} dispatch={dispatch} />
+      <h2 className="mainTitle">Drink Responsibly</h2>
       <img className='beerGuys' src='https://cloud.githubusercontent.com/assets/13898345/21078169/1f528acc-bfcb-11e6-8977-de6fa5b1c198.gif' />
     </div>
   )
