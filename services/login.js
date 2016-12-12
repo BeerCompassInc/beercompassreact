4a3943f69e6cd3b1ac9369cf64726cf4a297dbc3
Simon Casey <simon.jk.casey@gmail.com>
2016-12-12 13:49:48 +1300

    changed routes to new API routes

--- services/login.js
+++ services/login.js
@@ -3,7 +3,7 @@ import request from 'superagent'
 module.exports = ({loginDetails}, dispatch) => {
   const {username, password} = loginDetails
   request
-    .post('http://192.168.1.54:3000/login')
+    .post('http://localhost:3000/api/v1/login')
     .send({username, password})
     .withCredentials()
     .end((err,res) => {
