8a3ea2318c4de2f588d88aad8b5b3ab18693bcdc
chris-daynes <chrisdaynes@gmail.com>
2016-12-13 14:19:07 +1300

    start of this

--- services/trackAdventureService.js
+++ services/trackAdventureService.js
@@ -1,67 +1,3 @@
-// /* global google */
-// import React, { Component} from "react";
-//
-// import {
-//   withGoogleMap,
-//   GoogleMap,
-//   DirectionsRenderer,
-// } from "../../../lib";
-//
-// const DirectionsExampleGoogleMap = withGoogleMap(props => (
-//   <GoogleMap
-//     defaultZoom={7}
-//     defaultCenter={props.center}
-//   >
-//     {props.directions && <DirectionsRenderer directions={props.directions} />}
-//   </GoogleMap>
-// ));
-//
-// /*
-//  * Add <script src="https://maps.googleapis.com/maps/api/js"></script> to your HTML to provide google.maps reference
-//  */
-// export default class DirectionsExample extends Component {
-//
-//   state = {
-//     origin: new google.maps.LatLng(41.8507300, -87.6512600),
-//     destination: new google.maps.LatLng(41.8525800, -87.6514100),
-//     directions: null,
-//   }
-//
-//   componentDidMount() {
-//     const DirectionsService = new google.maps.DirectionsService();
-//
-//     DirectionsService.route({
-//       origin: this.state.origin,
-//       destination: this.state.destination,
-//       travelMode: google.maps.TravelMode.DRIVING,
-//     }, (result, status) => {
-//       if (status === google.maps.DirectionsStatus.OK) {
-//         this.setState({
-//           directions: result,
-//         });
-//       } else {
-//         console.error(`error fetching directions ${result}`);
-//       }
-//     });
-//   }
-//
-//   render() {
-//     return (
-//       <DirectionsExampleGoogleMap
-//         containerElement={
-//           <div style={{ height: `100%` }} />
-//         }
-//         mapElement={
-//           <div style={{ height: `100%` }} />
-//         }
-//         center={this.state.origin}
-//         directions={this.state.directions}
-//       />
-//     );
-//   }
-// }
-
-
 import React, { Component } from 'react'
 
 export default class extends Component {
@@ -95,9 +31,6 @@ export default class extends Component {
     const waypts = [
       {location: point1, stopover: true}]
 
-
-
-
         directionsService.route({
           origin: start,
           destination: end,
@@ -111,10 +44,7 @@ export default class extends Component {
               directionsDisplay.setDirections(res)
             }
           })
-
   }
-
-
   render() {
     return(
       <div id='map' ref='map' style={{width: '100vw', height: '89vh'}}></div>
